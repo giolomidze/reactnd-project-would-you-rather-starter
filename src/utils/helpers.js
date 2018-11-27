aa9e56082811f8394d31d3f351247219d5223cfa
Giorgi Lomidze <gio.lomidze@gmail.com>
2018-11-26 19:38:27 -0500

    feat: move generateUID function to helpers file

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -23,3 +23,14 @@ export function getQuestionInfo(users, question) {
     avatarUrl,
   };
 }
+
+export function generateUID() {
+  return (
+    Math.random()
+      .toString(36)
+      .substring(2, 15) +
+    Math.random()
+      .toString(36)
+      .substring(2, 15)
+  );
+}
