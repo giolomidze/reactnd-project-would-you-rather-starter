bc993289a4b9a33b2fe283b377e3fcc6d7abd362
Giorgi Lomidze <gio.lomidze@gmail.com>
2018-11-26 19:36:28 -0500

    feat: add navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Navigation extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/add">New Question</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Navigation;
