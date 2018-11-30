ef20619fccf0247ae7daa156ba0cd0445babcef1
Giorgi Lomidze <glomidze@kbra.com>
2018-11-30 14:25:57 -0500

    refactor: convert QuestionList component to stateless

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -2,23 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Question from './Question';
 
-class QuestionList extends Component {
-  render() {
-    const { questions } = this.props;
-    return (
-      <div>
-        <h3 className="center">Your Timeline</h3>
-        <ul>
-          {questions.map(question => (
-            <li key={question.id}>
-              <Question key={question.id} id={question.id} />
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+const QuestionList = props => {
+  return (
+    <div>
+      <h3 className="center">Your Timeline</h3>
+      <ul>
+        {props.questions.map(question => (
+          <li key={question.id}>
+            <Question key={question.id} id={question.id} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 function mapStateToProps({ questions, users, authedUser }, { id }) {
   const question = questions[id];
